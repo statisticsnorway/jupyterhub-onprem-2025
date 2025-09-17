c46ab133e7b351ae096b161f7b535a185eb3bdc2
PoyanAfifi <paf@ssb.no>
2025-09-17 19:32:43 +0200

    Refactor hub-control-panel-same-tab labextension build process
    This commit updates the build process for the 'hub-control-panel-same-tab' labextension by replacing the webpack build command with TypeScript compilation. Additionally, it removes the outdated webpack configuration file, streamlining the development setup. The package.json is also updated to include type definitions, enhancing type support for the extension.

--- docker/jupyterhub/labextensions/hub-control-panel-same-tab/webpack.config.js
+++ docker/jupyterhub/labextensions/hub-control-panel-same-tab/webpack.config.js
@@ -1,24 +1,4 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/index.ts',
-  output: {
-    path: path.resolve(__dirname, 'lib'),
-    filename: 'index.js',
-    libraryTarget: 'umd'
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  }
-};
+// Not used anymore; we compile with TypeScript only
+module.exports = {};
 
 
