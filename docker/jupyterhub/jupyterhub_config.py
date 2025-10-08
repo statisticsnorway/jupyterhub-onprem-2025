bae5fceb5508a5d44998df8a9b566679053afd7f
PoyanAfifi <paf@ssb.no>
2025-10-08 14:24:55 +0200

    whoops

--- docker/jupyterhub/jupyterhub_config.py
+++ docker/jupyterhub/jupyterhub_config.py
@@ -20,7 +20,7 @@ c.PAMAuthenticator.open_sessions = False
 
 c.Authenticator.allow_all = True
 
-c.DockerSpawner.pull_policy = "always
+c.DockerSpawner.pull_policy = "always"
 
 # Add admin users
 c.PAMAuthenticator.admin_groups = {"RBAG_jupyterhub_admins@ssb.no"}
